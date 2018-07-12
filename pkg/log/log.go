04ca59969a5015ac40a3499f7242ae316262a464
Italo Vietro <italo.vietro@n26.com>
2018-07-12 15:42:55 +0200

    Added missing log

---
+++ pkg/log/log.go
@@ -0,0 +1,39 @@
+package log
+
+import (
+	"context"
+
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/cli"
+)
+
+type loggerKeyType int
+
+const loggerKey loggerKeyType = iota
+
+var logger *log.Logger
+
+func init() {
+	log.SetHandler(cli.Default)
+	if l, ok := log.Log.(*log.Logger); ok {
+		logger = l
+	}
+}
+
+// NewContext returns a context that has a logrus logger
+func NewContext(ctx context.Context) context.Context {
+	return context.WithValue(ctx, loggerKey, WithContext(ctx))
+}
+
+// WithContext returns a logrus logger from the context
+func WithContext(ctx context.Context) *log.Logger {
+	if ctx == nil {
+		return logger
+	}
+
+	if ctxLogger, ok := ctx.Value(loggerKey).(*log.Logger); ok {
+		return ctxLogger
+	}
+
+	return logger
+}
---
+++ pkg/log/log_test.go
@@ -0,0 +1,48 @@
+package log
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMiddleware(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		scenario string
+		function func(*testing.T)
+	}{
+		{
+			scenario: "create log context",
+			function: testCreateLogContext,
+		},
+		{
+			scenario: "create log context with nil context",
+			function: testCreateLogWithNilContext,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.scenario, func(t *testing.T) {
+			test.function(t)
+		})
+	}
+}
+
+func testCreateLogContext(t *testing.T) {
+	ctx := NewContext(context.Background())
+	assert.NotNil(t, ctx)
+
+	logger := WithContext(ctx)
+	assert.NotNil(t, logger)
+}
+
+func testCreateLogWithNilContext(t *testing.T) {
+	ctx := NewContext(nil)
+	assert.NotNil(t, ctx)
+
+	logger := WithContext(nil)
+	assert.NotNil(t, logger)
+}
