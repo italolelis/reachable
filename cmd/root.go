43256bb55bd686b3abc37e7bbf55c5a05e7466af
italolelis <italolelis@gmail.com>
2021-04-26 15:48:45 +0200

    Simplify root command

--- cmd/root.go
+++ cmd/root.go
@@ -2,25 +2,22 @@ package cmd
 
 import (
 	"context"
-	"time"
 
-	"github.com/apex/log"
-	logCtx "github.com/italolelis/reachable/pkg/log"
+	log "github.com/italolelis/reachable/pkg/log"
 	"github.com/spf13/cobra"
 )
 
 type (
-	// RootOptions represents the ahoy global options
+	// RootOptions represents the reachable global options
 	RootOptions struct {
-		timeout time.Duration
 		verbose bool
 	}
 )
 
 // NewRootCmd creates the root command
-func NewRootCmd() *cobra.Command {
-	opts := RootOptions{}
-	ctx := logCtx.NewContext(context.Background())
+func NewRootCmd(ctx context.Context) *cobra.Command {
+	var opts RootOptions
+	ctx = log.NewContext(ctx)
 
 	cmd := cobra.Command{
 		Use:   "reachable",
@@ -34,16 +31,15 @@ func NewRootCmd() *cobra.Command {
 		`,
 		PersistentPreRun: func(ccmd *cobra.Command, args []string) {
 			if opts.verbose {
-				log.SetLevel(log.DebugLevel)
+				log.SetLevel("debug")
 			}
 		},
 	}
 
-	cmd.PersistentFlags().DurationVarP(&opts.timeout, "timeout", "t", 30*time.Second, "Defines a timeout")
 	cmd.PersistentFlags().BoolVarP(&opts.verbose, "verbose", "v", false, "Make the operation more talkative")
 
 	// Aggregates Root commands
-	cmd.AddCommand(NewCheckCmd(ctx, opts.timeout))
+	cmd.AddCommand(NewCheckCmd(ctx))
 	cmd.AddCommand(NewVersionCmd(ctx))
 
 	return &cmd
