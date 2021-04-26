e2f55be79950ada3b2f9bd770d940028d7691ca0
italolelis <italolelis@gmail.com>
2021-04-26 15:49:11 +0200

    Add async multiple calls

--- pkg/reachable/reachable.go
+++ pkg/reachable/reachable.go
@@ -2,15 +2,26 @@ package reachable
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"sync"
 	"time"
 
-	"github.com/gojektech/heimdall/httpclient"
+	"github.com/italolelis/reachable/pkg/log"
 	httpstat "github.com/tcnksm/go-httpstat"
 )
+
+type Target struct {
+	URL    string
+	Method string
+}
+
+func NewTarget(url string) *Target {
+	return &Target{url, http.MethodGet}
+}
 
 // Reachable represents the response for a domain check
 type Reachable struct {
@@ -18,48 +29,73 @@ type Reachable struct {
 	Port       string
 	IP         string
 	StatusCode int
-	Response   httpstat.Result
+	Response   *httpstat.Result
 }
 
 // IsReachable checks if a domain is reachable
-func IsReachable(ctx context.Context, domain string, timeout time.Duration) (*Reachable, error) {
-	var result httpstat.Result
-
-	u, err := url.Parse(domain)
+func IsReachable(ctx context.Context, t *Target) (Reachable, error) {
+	u, err := url.Parse(t.URL)
 	if err != nil {
-		return nil, err
+		return Reachable{}, err
 	}
 
 	if u.Scheme == "" {
 		u.Scheme = "http"
 	}
 
-	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
-	if err != nil {
-		return nil, err
-	}
+	var result httpstat.Result
 
 	ctx = httpstat.WithHTTPStat(ctx, &result)
-	req = req.WithContext(ctx)
+	req, err := http.NewRequestWithContext(ctx, t.Method, u.String(), nil)
+	if err != nil {
+		return Reachable{}, err
+	}
 
-	c := httpclient.NewClient(httpclient.WithHTTPTimeout(timeout))
+	r := Reachable{
+		Domain:   u.Hostname(),
+		Port:     u.Port(),
+		Response: &result,
+	}
+
+	c := &http.Client{}
 	res, err := c.Do(req)
 	if err != nil {
-		return nil, err
+		return r, err
 	}
 
+	r.StatusCode = res.StatusCode
+
 	if _, err := io.Copy(ioutil.Discard, res.Body); err != nil {
-		return nil, err
+		return r, err
 	}
+
 	res.Body.Close()
 	result.End(time.Now())
 
-	r := Reachable{
-		Domain:     res.Request.URL.Hostname(),
-		Port:       res.Request.URL.Port(),
-		StatusCode: res.StatusCode,
-		Response:   result,
+	return r, nil
+}
+
+// IsReachableAsync checks if a domain is reachable in a async way
+func IsReachableAsync(ctx context.Context, ch chan<- Reachable, chErr chan<- error, urls ...string) {
+	logger := log.WithContext(ctx)
+
+	var wg sync.WaitGroup
+	for _, t := range urls {
+		wg.Add(1)
+		go func(url string) {
+			logger.Debugw("checking domain", "domain", url)
+			result, err := IsReachable(ctx, NewTarget(url))
+			if err != nil {
+				chErr <- fmt.Errorf("unreachable: %w", err)
+			}
+			logger.Debugw("Reachable!", "domain", url)
+
+			ch <- result
+
+			wg.Done()
+		}(t)
 	}
 
-	return &r, nil
+	wg.Wait()
+	close(ch)
 }
